c6a4114aac7b2e007af26f27be186d5775640825
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-20 12:09:09 +0530

    feat: add Access Management page with user filters, table, and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "./pages/notFound/NotFound";
 import UserManagement from "./pages/UserManagement";
+import AccessManagement from "./pages/AccessManagement";
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/users" replace />} />
+        <Route path="/" element={<Navigate to="/access" replace />} />
         <Route path="/users" element={<UserManagement />} />
+        <Route path="/access" element={<AccessManagement />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ src/pages/AccessManagement.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Filters from '../components/Filters/Filters';
+import UserTable from '../components/UserTable/UserTable';
+import Pagination from '../components/Pagination/Pagination';
+import DashboardLayout from '../layouts/DashboardLayout';
+import { usersData } from '../utils/constant/data';
+
+export default function AccessManagement() {
+    const [search, setSearch] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const usersPerPage = 5;
+    return (
+        <DashboardLayout>
+            <div className="mb-4">
+                <h2 className="text-xl font-semibold  font-nunito">User Management</h2>
+                <p className="text-sm font-extralight text-gray-500">Manage your team members and their account permissions here.</p>
+            </div>
+
+            <Filters search={search} setSearch={setSearch} />
+            <UserTable users={usersData} />
+            <Pagination currentPage={currentPage} totalPages={Math.ceil(usersData.length / usersPerPage)} setCurrentPage={setCurrentPage} />
+        </DashboardLayout>
+    );
+}
