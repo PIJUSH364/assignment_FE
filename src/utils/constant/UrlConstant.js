51a19ce9e41c75a98db197644e879a194f61ec6c
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-25 11:57:43 +0530

    fix: update API_BASE_URL to use import.meta.env for improved environment variable handling

--- src/utils/constant/UrlConstant.js
+++ src/utils/constant/UrlConstant.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = `${process.env.VITE_BASE_BE_URL}/api/v1/`;
+const API_BASE_URL = `${import.meta.env.VITE_BASE_URL}/api/v1/`;
 const USER = "user";
 
 export const API_URLS = {
--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import stdLibBrowser from "vite-plugin-node-stdlib-browser";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     stdLibBrowser(), // Add the plugin to polyfill Node.js built-ins
   ],
-  define: {
-    "process.env": {
-      VITE_BASE_BE_URL: "https://contact-management-jw8v.onrender.com",
-    }, // Mock process object if needed
-  },
-  base: "/",
 });
