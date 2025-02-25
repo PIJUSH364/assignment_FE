91ba7a1ab82c3469442d8d499676838949c26d3e
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-02-25 12:11:23 +0530

    fix: update project name to 't_app' and add homepage URL in package.json

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import stdLibBrowser from "vite-plugin-node-stdlib-browser";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     stdLibBrowser(), // Add the plugin to polyfill Node.js built-ins
   ],
+  base: "/",
 });
